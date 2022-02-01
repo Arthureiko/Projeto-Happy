fd4131fce4dbf8124f56ff3aab089b57e8fa6c69
igorheije <arthureiko29@gmail.com>
2022-02-01 16:17:44 -0300

    alteracao no landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -13,7 +13,7 @@ function Landing() {
 
         <main>
           <h1>Leve felicidade para o mundo!</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+          <p>Visite orfanatos e mude o dia de muitas crianças, bla.</p>
         </main>
 
         <div className="location">
